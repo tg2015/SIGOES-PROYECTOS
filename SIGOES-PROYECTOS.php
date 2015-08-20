5e6838eeaebd2ab8f307e94bdfbb54cada07611e
william.abarca01 <william.abarca01@gmail.com>
2015-08-20 17:51:03 -0600

    Cambio de nombre de archivo principal de plugin y del controller

---
+++ SIGOES-PROYECTOS.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: SIGOES-PROYECTOS
+Plugin URI: http://modulos.egob.sv
+Description: Plugin Adminstracion de PROYECTOS
+Author:  Equipo de desarrollo SIGOES
+Author URI: http://modulos.egob.sv
+
+*/
+require_once( 'includes/proyectos-controller.php' );
+?>
